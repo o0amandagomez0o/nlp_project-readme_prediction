8cd5952ade4e58d66b7e2fa8de8e095cd6b16bfc
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-05-09 20:15:49 -0500

    beginning modeling

---
+++ model.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy import stats
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
+
+def get_metrics(mod, X, y):
+    """
+    This function takes in:
+    - mod: model being used
+    - X: X_train/validate/test: split features
+    - y: target
+    
+    """
+    baseline_accuracy = (train.churn == 0).mean()
+    y_pred = mod.predict(X)
+    accuracy = mod.score(X, y)
+    conf = confusion_matrix(y, y_pred)
+    prfs = pd.DataFrame(precision_recall_fscore_support(y, y_pred), index=['precision', 'recall', 'f1-score', 'support'])
+    
+    print(f'''
+    BASELINE accuracy is: {baseline_accuracy:.2%}
+    The accuracy for our model is: {accuracy:.2%} 
+    ''')
+    return prfs
